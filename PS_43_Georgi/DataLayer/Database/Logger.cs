788effe62825e407a2b3238748db0d8bed10bfdf
georgibzhnv <92206798+georgibzhnv@users.noreply.github.com>
2024-04-02 23:49:15 +0300

    Add files via upload

---
+++ PS_43_Georgi/DataLayer/Database/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Database
+{
+    public class Logger
+    {
+        private readonly DatabaseContext _context;
+
+        public Logger(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Log(string message, string logLevel = "SUCCESFUL") 
+        {
+            if (string.IsNullOrWhiteSpace(logLevel))
+            {
+                logLevel = "SUCCESFUL"; 
+            }
+
+            var logEntry = new LogEntry
+            {
+                Timestamp = DateTime.Now,
+                Message = message,
+                LogLevel = logLevel
+            };
+
+            _context.LogEntries.Add(logEntry);
+            _context.SaveChanges();
+        }
+    }
+}
