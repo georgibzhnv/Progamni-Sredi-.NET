db1277aceb3a0368f37c89e4045064537c7340c4
georgibzhnv <92206798+georgibzhnv@users.noreply.github.com>
2024-04-11 22:42:25 +0300

    Add files via upload

---
+++ PS_43_Georgi/Database/LogEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Database
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Message { get; set; }
+        public string LogLevel { get; set; }
+    }
+}
