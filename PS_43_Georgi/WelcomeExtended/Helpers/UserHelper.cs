e5220bc8ec44b17b5a5165da3f4abe5419453777
georgibzhnv <92206798+georgibzhnv@users.noreply.github.com>
2024-04-11 22:54:28 +0300

    Add files via upload

---
+++ PS_43_Georgi/WelcomeExtended/Helpers/UserHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+using WelcomeExtended.Data;
+
+namespace WelcomeExtended.Helpers
+{
+    public static class UserHelper
+    {
+        public static string toString(this User user) {
+
+            return $"Username: {user.Name}, User Role: {user.Role.ToString()}";
+        }
+        public static string ValidateCredentials(string name, string password,UserData userData)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "The username cannot be empty.";
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                return "The password cannot be empty.";
+            }
+            bool isValidUser = userData.ValidateUser(name, password);
+            if (!isValidUser) {
+                return "Invalid username or password.";
+            }
+            return "Credentials are valid.";
+        }
+       }
+    }
