2b17040e138008652da9a20f59a6495313266116
georgibzhnv <92206798+georgibzhnv@users.noreply.github.com>
2024-04-11 22:57:35 +0300

    Add files via upload

---
+++ PS_43_Georgi/Welcome/Program.cs
@@ -0,0 +1,29 @@
+﻿using Welcome.Model;
+using Welcome.Others;
+using Welcome.View;
+using Welcome.ViewModel;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            User user = new User
+            {
+                Name = "Georgi",
+                Password = "121221111",
+                Role = UserRolesEnum.STUDENT,
+                Email = "gbozhinov17@gmail.com"
+            };
+
+            UserViewModel userViewModel = new UserViewModel(user);
+            UserView userView = new UserView(userViewModel);
+            userView.Display();
+            userView.DisplayEmail();
+
+        }
+    }
+}
